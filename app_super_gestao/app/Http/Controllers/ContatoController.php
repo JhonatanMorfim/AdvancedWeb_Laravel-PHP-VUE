73ba24062cc3baf014461438d553279b476fdfbe
JhonatanMorfim <41458708+JhonatanMorfim@users.noreply.github.com>
2023-10-11 17:36:33 -0300

    Class_8: Form adjusts

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
+        $motivo_contatos= MotivoContato::all();       
+        return view('site.contato', ['titulo' =>'Contato', 'motivo_contatos' => $motivo_contatos]);
+    }
 
-        var_dump($_POST);
-        return view('site.contato', ['titulo' =>'Contato']);
+    public function salvar(Request $request){
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000'
+        ];
+        
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome precisa ter no máximo 40 caracteres',
+            'nome.unique' => 'Esse nome já foi utilizado para uma mensagem anterior',
+            'email.email' => 'O e-mail inserido não é válido',
+            'mensagem.max' => 'Você ultrapassou o limite de caracteres para o campo de mensagens',
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+        $request->validate(
+                $regras,
+                $feedback
+        );
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
--- app_super_gestao/database/migrations/2023_10_11_193044_create_motivo_contatos_table.php
+++ app_super_gestao/database/migrations/2023_10_11_193044_create_motivo_contatos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterSiteContatosEmailColumnSize extends Migration
+class CreateMotivoContatosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class AlterSiteContatosEmailColumnSize extends Migration
      */
     public function up()
     {
-        //
-        Schema::table('site_contatos', function (Blueprint $table) {
-            $table->string('email', 80)->change();
+        Schema::create('motivo_contatos', function (Blueprint $table) {
+            $table->id();
+            $table->string('motivo_contato', 20);
+            $table->timestamps();
         });
     }
 
@@ -26,9 +27,6 @@ class AlterSiteContatosEmailColumnSize extends Migration
      */
     public function down()
     {
-        //
-        Schema::table('site_contatos', function (Blueprint $table) {
-            $table->string('email', 30)->change();
-        });
+        Schema::dropIfExists('motivo_contatos');
     }
 }
